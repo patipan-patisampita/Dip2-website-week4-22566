af427b005ff11d8c4050e1c7389d60ebece76de5
patipan-patisampita <patipan-patisampita@gmail.com>
2023-11-07 11:39:10 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <EventHandling /> -->
+    <Counter />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import EventHandling from './components/EventHandle'
+import Counter from './components/Counter.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // EventHandling,
+    Counter,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
